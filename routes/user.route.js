3afc86642b1f9efac32bb8060087f3f49d2de127
DangKhoa1999 <buidangkhoa357@gmail.com>
2021-01-07 13:09:45 +0700

    Check username exist with ajax

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,15 +5,22 @@ router.get('/register',(req,res) => {
     res.render("vwUser/register.hbs");
 });
 
-router.post('/register', async function (req, res){
+router.post('/checkUsername',(req,res) => {
     const userInDB = ["nguyenvana", "123", "Le Thi Cuc"];
+    var Exist = true;
     for (user of userInDB)
     {
-        if (req.body.Username == user)
+        if (req.body.username == user)
         {
-            return res.render("vwUser/register.hbs", {err: true});
+            //return res.render("vwUser/register.hbs", {err: true});            
+            return res.status(200).send(Exist);
         }
     }
+    Exist = false;
+    res.status(200).send(Exist);
+});
+
+router.post('/register', async function (req, res){
     var user = {
         userName: req.body.Username,
         Password: req.body.Password,
