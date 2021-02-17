a7242e1ceeb9794f41a5302256d837adb2cbab7a
DangKhoa1999 <buidangkhoa357@gmail.com>
2021-02-17 20:48:03 +0700

    complete assemble the login page

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,38 +8,37 @@ const Helper = require("../utils/helper");
 const config = require("../config/default.json");
 const mailer = require("../utils/mailer");
 
-router.all('/confirmmail',(req,res)=>res.render('vwUser/confirmmail',{fullPage:true}))
+router.all('/confirmmail', (req, res) => res.render('vwUser/confirmmail', { fullPage: true }))
 
-router.get('/register2',(req,res) => {
-    res.render("vwUser/register2.hbs",{fullPage:true});
+router.get('/register2', (req, res) => {
+    res.render("vwUser/register2.hbs", { fullPage: true });
 });
-router.get('/register',(req,res) => {
+router.get('/register', (req, res) => {
     res.render("vwUser/register.hbs");
 });
 
-router.post('/register/checkinfo', async (req,res) => {
+router.post('/register/checkinfo', async (req, res) => {
     var Data;
-    try{
-      Data = JSON.parse(req.body.Data);
+    try {
+        Data = JSON.parse(req.body.Data);
     }
-    catch (error)
-    {
-      return res.send(error.message);
+    catch (error) {
+        return res.send(error.message);
     }
 
     var Exist = true;
     //Check mail    
-    if (await UserModel.checkExistEmail(Data.data.Email))        
-        return res.status(200).send({Exist: Exist, err: 0});
-    
+    if (await UserModel.checkExistEmail(Data.data.Email))
+        return res.status(200).send({ Exist: Exist, err: 0 });
+
     //Check username
-    if (await UserModel.checkExistUsername(Data.data.Username))       
-        return res.status(200).send({Exist: Exist, err: 1});
+    if (await UserModel.checkExistUsername(Data.data.Username))
+        return res.status(200).send({ Exist: Exist, err: 1 });
     Exist = false;
     res.status(200).send(Exist);
 });
 
-router.post('/register', async function (req, res){
+router.post('/register', async function (req, res) {
     var passwordHash = bcrypt.hashSync(req.body.Password, config.authentication.saltRounds);
     var token = Math.floor(100000 + Math.random() * 900000);
     var getDateTimeNow = Helper.ConverDateTime(new Date());
@@ -62,55 +61,54 @@ router.post('/register', async function (req, res){
     var linkActive = `http://localhost:3000/active?token=${user.activeToken}`;
     mailer.sendActiveToken(user.Email, linkActive);
     console.log(user);
-    res.render("vwUser/register.hbs", {Email: req.body.Email, Success: true});
+    res.render("vwUser/register.hbs", { Email: req.body.Email, Success: true });
 });
 
-router.get('/active', async function (req, res){
+router.get('/active', async function (req, res) {
     const token = +req.query.token || -1;
     const rs = await UserModel.activeAccount(token);
-    if (rs.changedRows > 0)
-    {
+    if (rs.changedRows > 0) {
         return res.send("Active Success");
     }
     res.send("Active Failed");
-    
+
 });
 
 
 router.get('/forget', (req, res) => {
-    res.render('vwUser/forget',{fullPage:true})
+    res.render('vwUser/forget', { fullPage: true })
 })
 
 router.post('/forget', (req, res, next) => {
-    const {email} = req.body
-    let randint,forgetToken
+    const { email } = req.body
+    let randint, forgetToken
     UserModel.getByEmai(email)
-        .then(users=>{
-            if (users.length == 0){
-                res.render('vwUser/forget',{error:"Không tìm thấy địa chỉ Email",model:{email},fullPage:1})
+        .then(users => {
+            if (users.length == 0) {
+                res.render('vwUser/forget', { error: "Không tìm thấy địa chỉ Email", model: { email }, fullPage: 1 })
                 throw true
             }
             const user = users[0]
-            randint = Math.floor(Math.random()*899999) + 100000
-            forgetToken = md5(user.ID+ '|' + randint)
+            randint = Math.floor(Math.random() * 899999) + 100000
+            forgetToken = md5(user.ID + '|' + randint)
 
-            UserModel.setForgetToken(forgetToken,user.ID)
-            return mailer.sendForgetToken(user.email,`${config.site.url}/forget/${forgetToken}?pass=${randint}`)
+            UserModel.setForgetToken(forgetToken, user.ID)
+            return mailer.sendForgetToken(user.email, `${config.site.url}/forget/${forgetToken}?pass=${randint}`)
         })
-        .then(response=>{
+        .then(response => {
             console.log('Email response:', response)
             console.log(`/forget/${forgetToken}?pass=${randint}`)
             return res.redirect('/confirmmail')
         })
-        .catch((e)=>e===true||next(e))
+        .catch((e) => e === true || next(e))
 })
 
-const forgetPasswordMiddleware = (req,res,next)=>{
-    const {token} = req.params
-    const {pass} = req.query
-    UserModel.getByForgetToken(token).then(response=>{
+const forgetPasswordMiddleware = (req, res, next) => {
+    const { token } = req.params
+    const { pass } = req.query
+    UserModel.getByForgetToken(token).then(response => {
         const user = response[0] || {}
-        const hash = md5(user.ID+ '|' + pass)
+        const hash = md5(user.ID + '|' + pass)
         if (!user || token != hash) {
             return res.redirect('/login')
         }
@@ -119,52 +117,51 @@ const forgetPasswordMiddleware = (req,res,next)=>{
     }).catch(next)
 }
 
-router.get('/forget/:token',forgetPasswordMiddleware,(req,res)=>{
-    res.render('vwUser/resetpassword',{fullPage:true})
+router.get('/forget/:token', forgetPasswordMiddleware, (req, res) => {
+    res.render('vwUser/resetpassword', { fullPage: true })
 })
 
-router.post('/forget/:token',forgetPasswordMiddleware,(req,res,next)=>{
-    const {p1,p2} = req.body
+router.post('/forget/:token', forgetPasswordMiddleware, (req, res, next) => {
+    const { p1, p2 } = req.body
     if (p1 != p2) {
-        return res.render('vwUser/resetpassword',{error:"Mật khẩu không khớp",fullPage:true})
+        return res.render('vwUser/resetpassword', { error: "Mật khẩu không khớp", fullPage: true })
     }
     var passwordHash = bcrypt.hashSync(req.body.p1, config.authentication.saltRounds);
 
     const id = req.targetUser.ID
-    const updateUser = {refreshToken:null,password:passwordHash,modifileDate:new Date()}
-    UserModel.patch(updateUser,id).then(()=>res.redirect('/login')).catch(next)
+    const updateUser = { refreshToken: null, password: passwordHash, modifileDate: new Date() }
+    UserModel.patch(updateUser, id).then(() => res.redirect('/login')).catch(next)
 })
 
-router.get('/login2', function (req, res) {
-    res.render('vwUser/login2',{fullPage:true})
-})
 router.get('/login', function (req, res) {
-
-  res.render('vwUser/login')
+    res.render('vwUser/login', { fullPage: true })
 })
-router.post('/login', async function(req, res){
-  const user=await UserModel.singleByUserNameorEmail(req.body.username);
-  if(user===null){
-    return res.render('vwUser/login',{
-      err: 'Invalid username or password.'
-    })
-  }
-  const rs = bcrypt.compareSync(req.body.password, user.password);
-  if (rs === false) {
-    return res.render('vwUser/login', {
-      err: 'Invalid username or password.'
-    })
-  }
-  if(user.status===0){
-    var linkActive = `http://localhost:3000/active?token=${user.activeToken}`;
-    mailer.sendActiveToken(user.email, linkActive);
-  }
-  delete user.password;
-  req.session.isAuthenticated = true;
-  req.session.authUser = user;
 
-  const url = req.query.retUrl || '/';
-  res.redirect(url);
+router.post('/login', async function (req, res) {
+    const user = await UserModel.singleByUserNameorEmail(req.body.username);
+    if (user === null) {
+        return res.render('vwUser/login', {
+            fullPage: true,
+            err: 'Invalid username or password.'
+        })
+    }
+    const rs = bcrypt.compareSync(req.body.password, user.password);
+    if (rs === false) {
+        return res.render('vwUser/login', {
+            fullPage: true,
+            err: 'Invalid username or password.'
+        })
+    }
+    if (user.status === 0) {
+        var linkActive = `http://localhost:3000/active?token=${user.activeToken}`;
+        mailer.sendActiveToken(user.email, linkActive);
+    }
+    delete user.password;
+    req.session.isAuthenticated = true;
+    req.session.authUser = user;
+
+    const url = req.query.retUrl || '/';
+    res.redirect(url);
 })
 
 module.exports = router;
