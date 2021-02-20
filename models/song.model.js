ed4657fd5eb5eac88ec8b94e465c3d786872a4e3
DangKhoa1999 <buidangkhoa357@gmail.com>
2021-02-20 18:17:51 +0700

    Complete page comment

--- models/song.model.js
+++ models/song.model.js
@@ -1,9 +1,16 @@
 const db=require("../utils/dao");
 
 const TBL_SONG="Songs";
-
+const TBL_Users_Comments = "Users_Comments";
 module.exports={
     search: function(searchString){
         return db.load(`SELECT * FROM ${TBL_SONG} WHERE Name LIKE '${searchString}'`);
+    },
+    addComment: function(entity){
+        return db.add(TBL_Users_Comments, entity);
+    },
+    getCommentListById: function(id){
+        return db.load(`SELECT us.ID, us.userName, us.avatar, usCM.content 
+        FROM ${TBL_Users_Comments} usCM JOIN users us ON usCM.User = us.ID WHERE usCM.Song = ${id} and usCM.delete = 0`);
     }
 }
--- routes/music.route.js
+++ routes/music.route.js
@@ -1,9 +1,38 @@
 const express = require("express");
+const { now } = require("moment");
+const songModel = require("../models/song.model");
 const router = express.Router();
+const SongModel = require("../models/song.model");
 
-router.get('/:id',(req, res) => {
+router.get('/:id', async (req, res) => {
     const id = +req.params.id || 1;
-    res.render("../views/vwMusic/detailMusic.hbs",{playable:true});
+    const listComment = await songModel.getCommentListById(id);
+    res.render("../views/vwMusic/detailMusic.hbs",{playable:true, idMusic: id, listComment});
+});
+
+router.post('/addcomment', async (req, res) => {
+    var info = "";
+    try{
+      info = JSON.parse(req.body.info);
+    }
+    catch (error)
+    {
+      console.error(error.message);
+    }
+    const entity = {
+      User: res.locals.lcAuthUser.ID,
+      Song: info.dataToSend.idMusic,
+      content: info.dataToSend.comment,
+      delete: 0,
+      createDate: new Date(),
+      modifileDate: new Date()
+    }
+    const rs = await SongModel.addComment(entity);
+    if (rs.affectedRows > 0)
+    {
+        return res.status(200).send(true);
+    }
+    res.status(200).send(false); 
 });
 
 
