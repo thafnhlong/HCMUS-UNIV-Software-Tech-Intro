7eb9edb6679c8a52f2572d349ad610541f75ba70
thafnhlong <thafnhlong@gmail.com>
2021-02-26 22:38:17 +0700

    MUZIK-54

--- routes/songs/searchs.js
+++ routes/songs/searchs.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const SongModel = require('../../models/song.model');
+const config = require('../../config/default.json')
+const songModel = require('../../models/song.model');
+const helper = require("../../utils/helper");
+const limitSong = config.pagination.limitSong
 
 
-router.get("/search/:searchString",async (req,res)=>{
-    const list = await SongModel.search(req.params.searchString);
-    res.render("vwSong/list.hbs", {
-        songs:list,
-        empty:list.length===0
-    });
+router.get("/search/:searchString",async (req,res,next)=>{
+    const {searchString} = req.params
+    const page = +req.query.page || 1;
+    const offset = (page - 1) * limitSong;
+
+    Promise.all([
+        songModel.search(searchString,true),
+        songModel.search(searchString,false,limitSong,offset)
+    ]).then(([[{numOfSong}],songList])=>{
+        const nPages = Math.ceil(numOfSong / limitSong);
+        const Pagination = helper.Pagination(nPages, page);
+        console.log(numOfSong,Pagination)
+        res.render("vwSong/list.hbs", {
+            searchString,
+            songList,
+            numOfSong,
+            Pagination
+        });
+    })
+    .catch(next)
 });
 
+router.get('/search', function (req, res, next) {
+    if (req.query.keyword)
+        return res.redirect(`/search/${req.query.keyword}`)
+    next()
+})
 module.exports=router;
