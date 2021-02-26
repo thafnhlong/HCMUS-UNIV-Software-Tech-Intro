eb817d1cde649dffbc4f263f92fd3369e40901b6
DangKhoa1999 <buidangkhoa357@gmail.com>
2021-02-26 21:11:47 +0700

    Complete favorite song

--- models/song.model.js
+++ models/song.model.js
@@ -3,6 +3,7 @@ const db=require("../utils/dao");
 const TBL_SONG="Songs";
 const TBL_Users_Comments = "Users_Comments";
 const TBL_Users = "Users";
+const TBL_Users_Favorite_Songs = "Users_Favorite_Songs";
 module.exports={
     add: function(song){
         return db.add(TBL_SONG, song);
@@ -25,5 +26,19 @@ module.exports={
         WHERE usCM.User = ${idUser} And usCM.Song = ${idSong}
         ORDER BY usCM.createDate DESC LIMIT 1`);
         return commentInfo[0];
+    },
+    getFavoriteListByUserId: function(userId){
+        return db.load(`SELECT usFS.ID, s.ID as songId, s.Name, us.userName 
+        FROM ${TBL_Users_Favorite_Songs} usFS JOIN ${TBL_SONG} s ON usFS.Song = s.ID JOIN ${TBL_Users} us ON s.author = us.ID 
+        WHERE usFS.User = ${userId} AND usFS.delete = 0 AND s.delete = 0`);
+    },
+    deleteFavoriteSongById: function(id){
+        const entity = {
+            delete: 1
+        }
+        const conditions = {
+            ID: id
+        }
+        return db.patch(TBL_Users_Favorite_Songs, entity, conditions);
     }
 }
--- routes/favorite.route.js
+++ routes/favorite.route.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const SongModel = require("../models/song.model");
 
-router.get('/', (req, res) => {
-    res.render('../views/vwMusic/favorite.hbs')
+router.get('/', async (req, res) => {
+    const userId = res.locals.lcAuthUser.ID;
+    const favoriteList = await SongModel.getFavoriteListByUserId(userId);
+    res.render('../views/vwMusic/favorite.hbs', {favoriteList})
 });
+
+router.post('/delete', async (req, res) => {
+    const id = req.body.id;
+    const rs = await SongModel.deleteFavoriteSongById(id);
+    let status = true;
+    if (rs.affectedRows === 0){
+        status = false;
+    }
+    res.status(200).send(status); 
+});
+
 
 
 
